5a3b2c70b0ed1f4a7f356ee1916fd874bd144d3f
Mai Trung Hieu (Mathew) <95751244+mhiuuu@users.noreply.github.com>
2022-10-03 20:54:14 +0700

    Create 268. Missing Number.py

---
+++ 268. Missing Number.py
@@ -0,0 +1,34 @@
+"""
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+"""
+
+#Answer:
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return int(n*(n+1)/2 - sum(nums))
+
+"""
+Runtime 117 ms
+Beats 99.96% 
+Memory 15.2 MB
+Beats 79.78%
+"""
