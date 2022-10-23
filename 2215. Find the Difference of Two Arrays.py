6ea6f174c82351bdcf7a3492aec8bcd93abe0f21
Mai Trung Hieu (Mathew) <95751244+mhiuuu@users.noreply.github.com>
2022-10-23 18:30:59 +0700

    Create 2215. Find the Difference of Two Arrays.py

---
+++ 2215. Find the Difference of Two Arrays.py
@@ -0,0 +1,37 @@
+"""
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+Example 2:
+
+Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+Output: [[3],[]]
+Explanation:
+For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+"""
+#Answer:
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        arr = []
+        for i in words1:
+            if i in words2 and words1.count(i) == 1 and words2.count(i) == 1:
+                arr.append(i)
+        return len(arr)
+      
+"""
+Runtime 399 ms Beats 15.54%
+Memory 14.4 MB Beats 38.69%
+"""
